e921078d1e7367c28bb4e3e43314735516768785
Fouad <fouad@fouad-MacBookPro.(none)>
2013-12-05 12:58:26 +0100

    test eclipse

---
+++ sources/monopoly/evenements/PayerLoyer.java
@@ -0,0 +1,30 @@
+package monopoly.evenements ;
+
+import monopoly.jeu.Joueur ;
+import monopoly.jeu.Case ;
+import monopoly.proprietes.Propriete;
+
+public class PayerLoyer extends AbstractEvent
+{
+    private Propriete prop;
+    
+    public PayerLoyer(Propriete prop, String nom, Joueur cible)
+    {
+	super(nom, cible);
+	this.prop = prop;
+    }
+    
+    public void executer()
+    {
+    	if (this.cible.payer(this.prop.loyer()))
+    	{
+    		this.prop.proprietaire().verser(this.prop.loyer());
+    	}
+    	
+    }
+    
+    public static void main(String[] args)
+    {
+	
+    }
+}
