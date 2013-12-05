7287f82c25cdd438f0c81b9be9661955e09bc686
Fouad <fouad@fouad-MacBookPro.(none)>
2013-12-05 23:13:03 +0100

    commit foireux

--- sources/monopoly/proprietes/Monopole.java
+++ sources/monopoly/proprietes/Monopole.java
@@ -5,7 +5,12 @@ import monopoly.jeu.Case;
 
 public abstract class Monopole extends UnePropriete{
 
-    public boolean constructible(){
+    public Monopole(Case pos, int prix, Groupe groupe, int[] loyer) {
+		super(pos, prix, groupe, loyer);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean constructible(){
 	return false;
     }
 
